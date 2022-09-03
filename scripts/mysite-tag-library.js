76ac93180be1235989d61a28d7e416b16ba84396
Kiran S <codehub.kirans@gmail.com>
2022-09-03 19:22:01 +0530

    Added images

--- scripts/mysite-tag-library.js
+++ scripts/mysite-tag-library.js
@@ -4,3 +4,16 @@ import { MysiteFooter } from '/scripts/custom-tags/mysite-footer.js';
 const MysiteTags = { MysiteNavbar, MysiteFooter };
 
 export { MysiteTags };
+
+/* Store a reference to the profile img element */
+const profileImage = document.querySelector('img');
+
+profileImage.onclick = () => {
+    const profileImgSrc = profileImage.getAttribute('src');
+
+    if (profileImgSrc === 'images/Kiran-S-profile-pic.jpg') {
+        profileImage.setAttribute('src', 'images/Kiran-S-altprofile-pic.jpg');
+    } else {
+        profileImage.setAttribute('src', 'images/Kiran-S-profile-pic.jpg');
+    }
+}
