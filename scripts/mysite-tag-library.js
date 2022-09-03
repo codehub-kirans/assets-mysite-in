9a796557aba8789e648cdcbbd6f1acc9dcba6c4a
Kiran S <codehub.kirans@gmail.com>
2022-09-04 01:38:01 +0530

    Updated nav & footer

--- scripts/mysite-tag-library.js
+++ scripts/mysite-tag-library.js
@@ -2,6 +2,7 @@ import { MysiteNavbar } from '/scripts/custom-tags/mysite-navbar.js';
 import { MysiteFooter } from '/scripts/custom-tags/mysite-footer.js';
 
 const MysiteTags = { MysiteNavbar, MysiteFooter };
+let MyassetDomain = 'https://assets-kirans.vercel.app/';
 
 export { MysiteTags };
 
@@ -10,10 +11,11 @@ const profileImage = document.querySelector('img');
 
 profileImage.onclick = () => {
     const profileImgSrc = profileImage.getAttribute('src');
+    console.log(profileImgSrc);
 
-    if (profileImgSrc === 'images/Kiran-S-profile-pic.jpg') {
-        profileImage.setAttribute('src', 'images/Kiran-S-altprofile-pic.jpg');
+    if (profileImgSrc === `${MyassetDomain}images/Kiran-S-profile-pic.jpg`) {
+        profileImage.setAttribute('src', `${MyassetDomain}images/Kiran-S-altprofile-pic.jpg`);
     } else {
-        profileImage.setAttribute('src', 'images/Kiran-S-profile-pic.jpg');
+        profileImage.setAttribute('src', `${MyassetDomain}images/Kiran-S-profile-pic.jpg`);
     }
 }
