1b2395edd6c9e8b87dd331ae8f88196da61160d2
Kiran S <codehub.kirans@gmail.com>
2022-09-04 01:02:09 +0530

    Updated to use custom CSS

--- scripts/custom-tags/mysite-navbar.js
+++ scripts/custom-tags/mysite-navbar.js
@@ -1,5 +1,49 @@
 import { LitElement, html } from 'https://jspm.dev/lit-element@2';
+export class MysiteNavbar extends LitElement {
+    static get styles() {
+        return css`
+        /* Add a black background color to the top navigation */
+        .topnav {
+          background-color: #313131;
+          overflow: hidden;
+        }
 
+        /* Style the links inside the navigation bar */
+        .topnav a {
+        float: left;
+        color: #d7d7d7;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+        }
+
+        /* Change the color of links on hover */
+        .topnav a:hover {
+        background-color: #313131;
+        color: #ffffff;
+
+        /* Add a color to the active/current link */
+        .topnav a.active {
+        background-color: #04AA6D;
+        color: white;
+        }
+        `;
+    }
+    render() {
+        return html`
+        <div class="topnav">
+            <a class="active" href="/">Home</a>
+            <a href="https://in.linkedin.com/in/kiran-s">Contact</a>
+            <a href="https://in.linkedin.com/in/kiran-s">About</a>
+        </div>
+        `;
+    }
+}
+
+customElements.define('mysite-navbar', MysiteNavbar);
+/*
+Old Framework CSS
 export class MysiteNavbar extends LitElement {
     render() {
         return html`
@@ -18,4 +62,4 @@ export class MysiteNavbar extends LitElement {
 }
 
 customElements.define('mysite-navbar', MysiteNavbar);
-
+*/
